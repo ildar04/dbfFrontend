a9241f6623a995d004473d95613642715f1e7c5a
totskii <totskii@elewise.com>
2019-09-27 17:19:30 +0300

    new project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <span>todo</span>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
